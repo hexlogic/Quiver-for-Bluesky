2ce914d578fe5fd909167ad1d650c63b7da3b3a7
Hexlogic <h3xlog1c@gmail.com>
2024-11-02 23:30:43 +0200

    Initial Commit

---
+++ Quiver for Bluesky/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Quiver for Bluesky
+//
+//  Created by Yevhen Lavrynenko on 02.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
